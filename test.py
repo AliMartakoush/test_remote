00fe7a45f61dc8314882b0bc08fd138ce40ef041
Ali Martakoush <alimartakoush@gmail.com>
2024-08-26 13:45:01 -0400

    Update test.py

--- test.py
+++ test.py
@@ -1,5 +1,19 @@
 import pygame
 import numpy as np 
 
-print(" if you're watching this you're gayأخي بالله أ
+print(" if you're watching this you're gayأخي بالله )
 
+
+
+
+
+
+
+
+
+
+
+
+
+      print("das versthehe ich nicht konnen sie das bitte buchstabieren ?)
+            
